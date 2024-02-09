6a48071d157b27c5703a1c763f8aa3e0a8f96c8b
Siddesh <siddeshshinde1214@gmail.com>
2024-02-10 01:13:45 +0530

    env add

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+const userRoutes = require("./routes/User");
+const quizRoutes = require("./routes/Quiz");
 
-const userRoutes = require('./routes/User');
-const quizRoutes = require('./routes/Quiz');
-
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = process.env.PORT;
 
-const { cloudinaryConnect } = require('./config/cloudinary');
-const fileUpload=require("express-fileupload");
-const database = require('./config/database');
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const database = require("./config/database");
 database.connect();
 
 app.use(express.json());
@@ -24,32 +23,32 @@ app.use(cookieParser());
 //     credentials:true,
 // }))
 
-app.use(cors({
-  credentials: true,
-  origin:  process.env.REACT_URL,
-}));
+app.use(
+	cors({
+		origin: process.env.REACT_URL,
+		credentials: true,
+	})
+);
 
-app.use(fileUpload(
-  {
-    useTempFiles: true,
-    tempFileDir:'/tmp'
-  }
-))
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: "/tmp",
+	})
+);
 
 cloudinaryConnect();
 
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/quiz", quizRoutes);
 
-app.use('/api/v1/auth', userRoutes);
-app.use('/api/v1/quiz', quizRoutes);
-
-app.get('/', (req, res) => {
-  return res.json({
-    success: true,
-    message: "Your server is running"
-  });
-})
-
+app.get("/", (req, res) => {
+	return res.json({
+		success: true,
+		message: "Your server is running",
+	});
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-})
+	console.log(`Server started on port ${PORT}`);
+});
